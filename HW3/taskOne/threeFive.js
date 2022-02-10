0aa90930a654f28500f9cdb1e5bce3d12565b8d8
YehorZakharov <yehor.zakharovv@gmail.com>
2022-02-10 18:46:30 +0200

    Update HW3 2.0

--- HW3/taskOne/threeFive.js
+++ HW3/taskOne/threeFive.js
@@ -9,8 +9,8 @@ readline.question("Enter num1: ", (num1) => {
       readline.question("Enter num4: ", (num4) => {
         let arr = [];
         arr.push(num1, num2, num3, num4);
-        for (let i of arr) {
-          if (arr[i] % 3 === 0 || arr[i] % 5 === 0) {
+        for (let i in arr) {
+          if (arr[i] % 3 == 0 || arr[i] % 5 == 0) {
             console.log(Number(arr[i]));
           }
         }
