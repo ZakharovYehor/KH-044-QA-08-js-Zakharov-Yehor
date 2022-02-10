b15eee1fbde4b5e222411132b8acd78d913ee1d2
YehorZakharov <yehor.zakharovv@gmail.com>
2022-02-11 00:38:24 +0200

    Update H3 - taskTwo - string.js

--- HW3/taskTwo/string.js
+++ HW3/taskTwo/string.js
@@ -3,10 +3,27 @@ const readline = require("readline").createInterface({
   output: process.stdout,
 });
 
-readline.question("Enter year: ", (str) => {
-  if (str !== "") {
-    console.log("This string is not empty");
-  } else {
-    console.log("This string is empty");
+readline.question("Enter string: ", (str) => {
+  let name = "";
+  let symbol = "qwertyuiopasdfghjklzxcvbnm";
+  let number = "0123456789";
+  let specialSymbol = "(!#$%&'()*+,-./:;<=>?@[]^_`{|}~)";
+
+  function CountStr(name, sns, str) {
+    let count = 0;
+    let res = "";
+    for (let i = 0; i < str.length; i++) {
+      for (let j = 0; j < sns.length; j++) {
+        if (str[i] === sns[j]) {
+          count++;
+          res += str[i];
+        }
+      }
+    }
+    console.log(`${name} ${count} ${res}`);
   }
+
+  CountStr("Characters:", symbol, str);
+  CountStr("Numbers:", number, str);
+  CountStr("Special symbols:", specialSymbol, str);
 });
